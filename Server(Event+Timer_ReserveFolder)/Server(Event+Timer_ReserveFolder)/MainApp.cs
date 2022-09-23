da279a7b9846429c1eec2e9843123e803fb75315
VladimirOB <vovaso4@gmail.com>
2022-09-23 21:35:10 +0300

    serv

---
+++ Server(Event+Timer_ReserveFolder)/Server(Event+Timer_ReserveFolder)/MainApp.cs
@@ -0,0 +1,25 @@
+﻿namespace Server_Event_Timer_ReserveFolder_
+{
+    class MainApp
+    {
+        /*2. Сервер работает по таймеру и следит за определённой папкой и делает резервные копии этой папки в папке BackUp
+        Подписчики:
+        1) выводит информацию о файле и о действиях сервера на экран
+        2) останавливает сервер после 10 сек. работы
+        3) пишет в файл .log*/
+
+        static void Main()
+        {
+            Client1 client = new Client1();
+            Client2 client2 = new Client2();
+            Client3 client3 = new Client3();
+            Server server = new Server("V:\\temp");
+            server.Add(client.ShowInfo);
+            server.Add(client2.shutDownServer);
+            server.Add(client3.Log);
+
+            server.Start();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server(Event+Timer_ReserveFolder)/Server(Event+Timer_ReserveFolder)/Server.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Event_Timer_ReserveFolder_
+{
+
+    delegate int Subscriber(FileInfo current, uint timerCnt);
+
+    class Server
+    {
+        //переменная-списка адресов функций (event)
+        protected event Subscriber subscribers;
+        System.Timers.Timer timer;
+        DirectoryInfo dinfo;
+        DirectoryInfo dinfoResult;
+        HashSet<FileInfo> filesTemp;
+        HashSet<FileInfo> files;
+        uint timerCnt = 0;
+        string sourceDir;
+        string backupDir;
+        public Server(string path)
+        {
+            sourceDir = path;
+            backupDir = @"V:\BackUp";
+            dinfo = new DirectoryInfo(path);
+            dinfoResult = new DirectoryInfo(backupDir);
+            if (dinfoResult.Exists == false)
+                dinfoResult.Create();
+        }
+
+        public void Start()
+        {
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+        }
+
+        private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            timerCnt++;
+            Console.WriteLine($"Time: {timerCnt}");
+            StartEvent();
+        }
+
+        //метод, подписывающий клиентские классы на сообщ.
+        public void Add(Subscriber ev)
+        {
+            subscribers += ev;
+        }
+
+        public void StartEvent()
+        {
+            
+            filesTemp = dinfo.GetFiles("*.*", SearchOption.AllDirectories).ToHashSet();
+            files = dinfoResult.GetFiles("*.*", SearchOption.AllDirectories).ToHashSet();
+            foreach (FileInfo current in filesTemp)
+            {
+                if (!files.Any(a => a.Name == current.Name && a.Length == current.Length))
+                {
+                    files.Add(current);
+                    File.Copy(Path.Combine(sourceDir, current.Name), Path.Combine(backupDir, current.Name), true);
+                    
+                    subscribers?.Invoke(current, timerCnt);
+                }
+            }
+            if (timerCnt == 10)
+                Environment.Exit(0);
+        }
+    }
+
+    class Client1
+    {
+        // обработчик события
+        public int ShowInfo(FileInfo current, uint timerCnt)
+        {
+            int cnt = current.FullName.Length;
+            Console.Write($"Server copy {current.Name} to backUpDir");
+            for (int i = cnt; i < 40; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine($"size: {current.Length}");
+            return 0;
+        }
+    }
+
+
+    class Client2
+    {
+        // обработчик события
+        public int shutDownServer(FileInfo current, uint timerCnt)
+        {
+            if (timerCnt == 10)
+                Environment.Exit(0);
+            return 0;
+        }
+    }
+
+    class Client3
+    {
+        // обработчик события
+        public int Log(FileInfo current, uint timerCnt)
+        {
+            int cnt = current.FullName.Length;
+            using (StreamWriter writer = File.AppendText("../../../../server.log"))
+            {
+                writer.Write(current.FullName);
+                for (int i = cnt; i < 40; i++)
+                {
+                    writer.Write(" ");
+                }
+                writer.WriteLine($"size: {current.Length}");
+            }
+
+            return 0;
+        }
+
+    }
+}
